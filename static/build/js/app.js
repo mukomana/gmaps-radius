b6b6f4728d409bf625c20d06044128bb02193d93
mukomana <mukomanaf@yahoo.com>
2016-01-31 06:38:52 +0200

    Update app.js

--- static/build/js/app.js
+++ static/build/js/app.js
@@ -88,7 +88,7 @@ l=h.substring(0,l.length)!==l?g(""):new g(h.substring(l.length)),l._parentURI=th
     var flightPlanCoordinates = [
     	{lat: -26.44435859, lng: 29.6697794},//Davel
     	{lat: -26.4294017, lng: 29.7102435}, //Mooovlei
-    	{lat: -26.5104804, lng: 29.9120344},//Ermelo
+    	{lat: -26.53686, lng: 29.959116},//Ermelo
     	{lat: -26.483606, lng: 30.1170495},//Meppel
     	{lat: -26.4574952, lng: 30.1703113},//Burgerspan
     	{lat: -26.4547151, lng: 30.2605913},//koolbank
